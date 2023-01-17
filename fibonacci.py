6b3b8cea3735bf06b1b26b49fb9f4e4f3ce9e7fd
Karl Johnson <karlsjohnson@icloud.com>
2023-01-17 11:29:04 -0600

    Files

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+standard_input = input()
+num = int(standard_input)
+
+def fibonacci(n):
+	#complete the recursive function
+	if n <= 1:
+		return n
+
+	else:
+		return fibonacci(n-1) + fibonacci(n-2)
+
+
+
+for i in range(num):
+	print(fibonacci(i))
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 print("hello world")
 text = input("What is your name?")
 print("Hello " + text)
+
+
+print("next level")
